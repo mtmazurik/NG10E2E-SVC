276e9ba239caf1e26beafbb615ab9c579be4af00
Marty Mazurik <marty@martymazurik.com>
2021-12-03 13:29:01 -0800

    fix build

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen( c =>
-//{
-//    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "ng10e2e-svc", Version = "v1", });
-//}
+builder.Services.AddSwaggerGen( /* c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "ng10e2e-svc", Version = "v1", });
+} */
 );
 
 var app = builder.Build();
