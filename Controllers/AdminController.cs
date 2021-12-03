d60568bc1f3790ac7c6b56d4c6948448a044813b
Marty Mazurik <marty@martymazurik.com>
2021-12-03 09:37:24 -0800

    fix namespace, hyphens not allowed

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace ng10e2e-svc.Controllers
+namespace ng10e2esvc.Controllers
 {
     [Route("[controller]")] //   /admin
     [ApiController]
