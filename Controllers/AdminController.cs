5121b7870bb7d7b682173648457e9e4e533ed4b7
Marty Mazurik <marty@martymazurik.com>
2021-12-03 07:20:01 -0800

    initial commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NG10E2E_SVC.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        // GET: api/<AdminController>
+        [HttpGet("ping")]
+        public IActionResult Get()
+        {
+            return Ok("200 OK");
+        }
+    }
+}
